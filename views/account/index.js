a012799c9f0a0e7dc9c3276f7d01ed04140cdbef
LMVALDERRAMAC <lmvalderramac@uqvirtual.edu.co>
2017-05-09 09:11:01 -0500

    Se corrige error en dependencias de idioma

--- views/account/index.js
+++ views/account/index.js
@@ -1,9 +1,6 @@
 (function() {
   'use strict';
   exports.init = function(req, res){
-
-    res.setLocale(req.params.locale); //for current request
-    //res.cookie('idioma', req.params.locale); //for future request of that user
     res.render('account/index',{i18n: res});
   };
 }());
